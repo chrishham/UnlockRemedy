51e71a4adc752759008e0e77f46f80b86dc9c8a5
chamaletsos <chrishham@yahoo.gr>
2018-11-02 11:24:46 +0200

    unlock summary

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 /* global alert, location, setTimeout */
 
 (function () {
-  let a = document.getElementById('arid_WIN_3_10000101')
+  let notes = document.getElementById('arid_WIN_3_10000101')
+  let summary = document.getElementById('arid_WIN_3_8')
+  if (!notes) return alert('Δεν μπορεί να ξεκλειδώσει τα πάντα!')
+  unlock(notes)
+  unlock(summary)
   let saveBtn = document.getElementById('WIN_3_300000300')
-  if (!a) return alert('Μπορεί να ξεκλειδώσει μόνο notes στο Apply το Production!')
-  a.removeAttribute('readonly')
-  let imageUrl = 'https://chrishham.github.io/remedy/images/Ninja.jpg'
-  a.style['background-image'] = `url(${imageUrl})`
-  a.style['background-size'] = 'contain'
-  a.style['background-repeat'] = 'no-repeat'
-  a.style['background-position-x'] = 'right'
-  a.style['background-color'] = '#dbf1db'
-  a.style['border-radius'] = '10px'
-  a.style['padding-top'] = '10px'
-  a.placeholder = 'Εισάγετε το νέο κείμενο και μετά πατήστε save...'
-  alert('Επιτυχές ξεκλείδωμα notes!')
   saveBtn.addEventListener('click', () => {
     setTimeout(() => {
       console.log('i was clicked!')
@@ -23,3 +15,16 @@
   })
 }
 )()
+
+function unlock (a) {
+  let imageUrl = 'https://chrishham.github.io/remedy/images/Ninja.jpg'
+  a.removeAttribute('readonly')
+  a.style['background-image'] = `url(${imageUrl})`
+  a.style['background-size'] = 'contain'
+  a.style['background-repeat'] = 'no-repeat'
+  a.style['background-position-x'] = 'right'
+  a.style['background-color'] = '#dbf1db'
+  a.style['border-radius'] = '10px'
+  a.style['padding-top'] = '10px'
+  a.placeholder = 'Εισάγετε το νέο κείμενο και μετά πατήστε save...'
+}
