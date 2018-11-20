fe1d9edeeb98c254c1aa1a44c180291c7b15fd9c
chamaletsos <chrishham@yahoo.gr>
2018-11-20 13:04:38 +0200

    added babel

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-/* global alert, location, setTimeout */
+/* global setTimeout */
 
 (function () {
-  let notes = document.getElementById('arid_WIN_3_10000101')
-  let summary = document.getElementById('arid_WIN_3_8')
-  if (!notes) return alert('Δεν μπορεί να ξεκλειδώσει τα πάντα!')
-  unlock(notes)
-  unlock(summary)
-  let saveBtn = document.getElementById('WIN_3_300000300')
-  saveBtn.addEventListener('click', function () {
-    setTimeout(function () {
-      console.log('i was clicked!')
-      location.reload()
-    }, 1000)
-  })
+  let readOnlys = document.querySelectorAll('[readonly]')
+
+  readOnlys.forEach(el => {
+    let cursorUrl = 'https://chrishham.github.io/remedy/images/key.cur'
+    el.addEventListener('click', removeReadonly)
+    el.style['cursor'] = `url('${cursorUrl}'),crosshair`
+  }
+  )
 }
 )()
 
-function unlock (a) {
+function removeReadonly (event) {
+  let a = event.target
+  console.log(a)
   let imageUrl = 'https://chrishham.github.io/remedy/images/Ninja.jpg'
   a.removeAttribute('readonly')
-  a.style['background-image'] = 'url(' + imageUrl + ')'
+  a.style['background-image'] = `url(${imageUrl})`
   a.style['background-size'] = 'contain'
   a.style['background-repeat'] = 'no-repeat'
   a.style['background-position-x'] = 'right'
   a.style['background-color'] = '#dbf1db'
   a.style['border-radius'] = '10px'
   a.style['padding-top'] = '10px'
-  a.placeholder = 'Εισάγετε το νέο κείμενο και μετά πατήστε save...'
 }
