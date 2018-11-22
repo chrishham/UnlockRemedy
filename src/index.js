30a5459ff1d2114fc154b7a89d26aa58101591e8
chamaletsos <chrishham@yahoo.gr>
2018-11-22 08:42:57 +0200

    minor fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var isChrome = !!window.chrome && !!window.chrome.webstore;
 (function () {
   let readOnlys = document.querySelectorAll('[readonly]')
   console.log('isChrome : ' + isChrome)
+  if (!readOnlys) return
   readOnlys.forEach(el => {
     let cursorUrl = isChrome ? 'https://chrishham.github.io/remedy/images/key_24x24.png' : 'https://chrishham.github.io/remedy/images/key.cur'
     el.addEventListener('click', removeReadonly)
