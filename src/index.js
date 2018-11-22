619f411a75a3888a053440e1a668aea170fb7739
chamaletsos <chrishham@yahoo.gr>
2018-11-22 10:01:08 +0200

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 /* global setTimeout,location */
-import 'nodelist-foreach-polyfill'
-var isChrome = !!window.chrome && !!window.chrome.webstore;
+isChrome = !!window.chrome && !!window.chrome.webstore;
 (function () {
   let readOnlys = document.querySelectorAll('[readonly]')
   console.log('isChrome : ' + isChrome)
   if (!readOnlys) return
   readOnlys.forEach(el => {
-    let cursorUrl = isChrome ? 'https://chrishham.github.io/remedy/images/key_24x24.png' : 'https://chrishham.github.io/remedy/images/key.cur'
-    el.addEventListener('click', removeReadonly)
-    el.style['cursor'] = isChrome ? `url('${cursorUrl}') 25 15,crosshair` : `url('${cursorUrl}'),crosshair`
-    let saveBtn = document.getElementById('WIN_3_300000300')
-    saveBtn.addEventListener('click', () => {
-      setTimeout(() => {
-        console.log('i was clicked!')
-        location.reload()
-      }, 1000)
-    })
+    let previous = el.previousElementSibling
+    if (previous && /(Notes|Summary)/.test(previous.innerHTML)) {
+      console.log(el)
+      let cursorUrl = isChrome ? 'https://chrishham.github.io/remedy/images/key_24x24.png' : 'https://chrishham.github.io/remedy/images/key.cur'
+      el.addEventListener('click', removeReadonly)
+      el.style['cursor'] = isChrome ? `url('${cursorUrl}') 25 15,crosshair` : `url('${cursorUrl}'),crosshair`
+      let saveBtn = document.getElementById('WIN_3_300000300')
+      saveBtn.addEventListener('click', () => {
+        setTimeout(() => {
+          console.log('i was clicked!')
+          location.reload()
+        }
+          , 1000)
+      }
+      )
+    }
   })
 }
 )()
